e69bf48c12cb4bb703efe7fb0bc1fb45753bee26
asperin2@gmail.com <asperin2@gmail.com>
2016-08-31 14:46:00 +0300

    fix engine

--- import.php
+++ import.php
@@ -328,9 +328,16 @@ foreach ($this->groups as $group => $group_name) {
             }
     }
 
+    public function getLastToken($table) {
+        $mysqli2 = new mysqli($this->host_carddav, $this->db_user_carddav, $this->db_pass_carddav, $this->db_name_carddav);
+        $result = $mysqli2->query("SELECT MAX(synctoken) FROM ".$table." LIMIT 1");
+        return $result->fetch_field();
+
+    }
+
 
 }
 
 error_reporting(E_ALL);
 $obj = new convertToOwncloud;
-echo $obj->importDb() . "\r\n\r\n";
+echo $obj->getLastToken('oc_adressbooks') . "\r\n\r\n";
